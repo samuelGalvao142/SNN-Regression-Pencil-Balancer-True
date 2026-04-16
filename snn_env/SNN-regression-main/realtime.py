ffa8ef097c69adabb14c016992a81d218d712427
s244196 <s244196@dac.unicamp.br>
2026-04-16 16:09:18 -0400

    Modified main.py for dual SNN run, wrong branch (move mods to second-call)

--- snn_env/SNN-regression-main/realtime.py
+++ snn_env/SNN-regression-main/realtime.py
@@ -32,7 +32,7 @@ PI = math.pi
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = SNN_Net(
+xNetwork = SNN_Net(
     tau=CONFIG["tau"],
     final_tau=CONFIG["final_tau"],
     hidden=CONFIG["hidden"],
@@ -42,12 +42,28 @@ model = SNN_Net(
 )
 
 checkpoint = torch.load(r"C:\Users\sgalvao\snn_regression\models\model_SEW_BN\checkpoints_pendulum\checkpoint_best.pth", map_location=DEVICE)
-model.load_state_dict(torch.load(r"C:\Users\sgalvao\snn_regression\models\model_SEW_BN\checkpoints_pendulum\best_model_weights.pth"))
+xNetwork.load_state_dict(torch.load(r"C:\Users\sgalvao\snn_regression\models\model_SEW_BN\checkpoints_pendulum\best_model_weights.pth"))
 
-model.to(DEVICE)
-model.eval()
+xNetwork.to(DEVICE)
+xNetwork.eval()
 
-functional.reset_net(model)
+functional.reset_net(xNetwork)
+
+yNetwork = SNN_Net(
+    tau=CONFIG["tau"],
+    final_tau=CONFIG["final_tau"],
+    hidden=CONFIG["hidden"],
+    norm_type=CONFIG["norm_type"],
+    learnable_norm=CONFIG["learnable_norm"],
+    init_scale=CONFIG["init_scale"]
+)
+
+yNetwork.load_state_dict(torch.load(r"C:\Users\sgalvao\snn_regression\models\model_SEW_BN\checkpoints_pendulum\best_model_weights.pth"))
+
+yNetwork.to(DEVICE)
+yNetwork.eval()
+
+functional.reset_net(yNetwork)
 
 # =============================================================================
 # Testing with prerecorded footage
@@ -116,53 +132,84 @@ except KeyboardInterrupt:
 # =============================================================================
 # Testing with the event camera
 # =============================================================================
-capture = dv.io.camera.open()
+cameras = dv.io.camera.discover()
 
-capture.setEventsRunning(True)
-capture.setFramesRunning(True)
+xCapture = dv.io.camera.open(cameras[0])
+yCapture = dv.io.camera.open(cameras[1])
 
-if not capture.isEventStreamAvailable():
-    raise RuntimeError("No event camera detected")
+xCapture.setEventsRunning(True)
+xCapture.setFramesRunning(True)
 
-resolution = capture.getEventResolution()
+yCapture.setEventsRunning(True)
+yCapture.setFramesRunning(True)
+
+if not xCapture.isEventStreamAvailable():
+    raise RuntimeError("No event camera detected (X axis)")
+
+if not yCapture.isEventStreamAvailable():
+    raise RuntimeError("No event camera detected (Y axis)")
+
+resolution = xCapture.getEventResolution()
 W, H = resolution
 print("Resolution:", resolution)
 
 print("Live inference started")
 
-while capture.isRunning():
+while xCapture.isRunning() and yCapture.isRunning():
 
     start = time.perf_counter()
 
-    events = capture.getNextEventBatch()
-    if events is None:
+    xEvents = xCapture.getNextEventBatch()
+    yEvents = yCapture.getNextEventBatch()
+    if xEvents is None or yEvents is None:
         continue
 
-    events_np = events.numpy()
+    xEvents_np = xEvents.numpy()
+    yEvents_np = yEvents.numpy()
 
-    xs = events_np['x']
-    ys = events_np['y']
-    ps = events_np['polarity']
+    x_xs = xEvents_np['x']
+    x_ys = xEvents_np['y']
+    x_ps = xEvents_np['polarity']
 
-    pos_mask = ps == 1
-    neg_mask = ps == 0
+    y_xs = yEvents_np['x']
+    y_ys = yEvents_np['y']
+    y_ps = yEvents_np['polarity']
 
-    pos_frame = np.zeros((H, W), dtype=np.float32)
-    neg_frame = np.zeros((H, W), dtype=np.float32)
+    x_pos_mask = x_ps == 1
+    x_neg_mask = x_ps == 0
 
-    pos_frame[ys[pos_mask], xs[pos_mask]] += 1
-    neg_frame[ys[neg_mask], xs[neg_mask]] += 1
+    y_pos_mask = y_ps == 1
+    y_neg_mask = y_ps == 0
 
-    frame = np.stack([pos_frame, neg_frame], axis=0)
-    frame = torch.from_numpy(frame).unsqueeze(0).to(DEVICE)
+    x_pos_frame = np.zeros((H, W), dtype=np.float32)
+    x_neg_frame = np.zeros((H, W), dtype=np.float32)
+    
+    y_pos_frame = np.zeros((H, W), dtype=np.float32)
+    y_neg_frame = np.zeros((H, W), dtype=np.float32)
+
+    x_pos_frame[x_ys[x_pos_mask], x_xs[x_pos_mask]] += 1
+    x_neg_frame[x_ys[x_neg_mask], x_xs[x_neg_mask]] += 1
+
+    y_pos_frame[y_ys[y_pos_mask], y_xs[y_pos_mask]] += 1
+    y_neg_frame[y_ys[y_neg_mask], y_xs[y_neg_mask]] += 1
+
+    xFrame = np.stack([x_pos_frame, x_neg_frame], axis=0)
+    xFrame = torch.from_numpy(xFrame).unsqueeze(0).to(DEVICE)
+
+    yFrame = np.stack([y_pos_frame, y_neg_frame], axis=0)
+    yFrame = torch.from_numpy(yFrame).unsqueeze(0).to(DEVICE)
 
     with torch.no_grad():
-        output = model(frame)
+        x_output = xNetwork(xFrame)
+        y_output = yNetwork(yFrame)
 
-    output_deg = output.item() * 180
+    x_output_deg = x_output.item() * 180
+    y_output_deg = y_output.item() * 180
 
     latency = (time.perf_counter() - start) * 1000
 
-    print(f"Prediction: {output.item():.4f} rad | {output_deg:.1f} degrees | Latency: {latency:.2f} ms")
+    print(f"x Axis Prediction: {x_output.item():.4f} rad | y Axis Prediction: {y_output.item():.4f} rad | Latency: {latency:.2f} ms")
 
-    out_port.write(f"{output_deg}\n".encode())
+
+    out_port.write(f"{x_output_deg}\n".encode())
+    out_port.write(f"{y_output_deg}\n".encode())
